b972471a7b24b59239993ce146d6a5016e398e8d
Thomas Mailund <mailund@birc.au.dk>
2022-04-07 10:26:37 +0200

    Removed solution

--- src/test_sieve.py
+++ src/test_sieve.py
@@ -2,10 +2,7 @@
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
-from sieve import (
-    sieve,
-    slow_sieve,
-)
+from sieve import sieve
 
 
 def test_sieve() -> None:
@@ -23,20 +20,3 @@ def test_sieve() -> None:
     assert sieve(11) == [2, 3, 5, 7, 11]
     assert sieve(12) == [2, 3, 5, 7, 11]
     assert sieve(13) == [2, 3, 5, 7, 11, 13]
-
-
-def test_slow_sieve() -> None:
-    """Test the slow implementation."""
-    assert slow_sieve(1) == []
-    assert slow_sieve(2) == [2]
-    assert slow_sieve(3) == [2, 3]
-    assert slow_sieve(4) == [2, 3]
-    assert slow_sieve(5) == [2, 3, 5]
-    assert slow_sieve(6) == [2, 3, 5]
-    assert slow_sieve(7) == [2, 3, 5, 7]
-    assert slow_sieve(8) == [2, 3, 5, 7]
-    assert slow_sieve(9) == [2, 3, 5, 7]
-    assert slow_sieve(10) == [2, 3, 5, 7]
-    assert slow_sieve(11) == [2, 3, 5, 7, 11]
-    assert slow_sieve(12) == [2, 3, 5, 7, 11]
-    assert slow_sieve(13) == [2, 3, 5, 7, 11, 13]
